8041c41ae35c9572f029e0f0034ad6b4ab1c4450
eder15720 <eder15720@hotmail.com>
2024-05-29 00:12:13 -0300

    adicionando itens da lista e formulário

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,7 +5,7 @@ import { RouterOutlet } from '@angular/router';
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
-  template: `1<router-outlet></router-outlet>2`
+  template: `<router-outlet></router-outlet>`
 })
 export class AppComponent {
   title = 'lista-de-tarefas';
